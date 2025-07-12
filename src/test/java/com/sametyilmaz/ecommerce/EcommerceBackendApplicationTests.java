9f4b190eebbeb9bf2577261754d5dc3a334c089c
Samet Yılmaz <sametylmz621@gmail.com>
2025-07-12 15:24:29 +0300

    initializing spring boot project

---
+++ src/test/java/com/sametyilmaz/ecommerce/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametyilmaz.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
